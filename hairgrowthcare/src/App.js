e3931bfc2b8e972ba70d97beb90c9c6f65b643bb
umbur <work1415@gmail.com>
2023-01-10 20:14:40 -0700

    first real commit

--- hairgrowthcare/src/App.js
+++ hairgrowthcare/src/App.js
@@ -1,12 +1,93 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import ReactDOM from "react-dom";
 
-function App() {
+const styles = {
+  container: {
+    textAlign: "center",
+    padding: "10px",
+  },
+  heading: {
+    fontSize: "32px",
+    color: "blue",
+  },
+  paragraph: {
+    fontSize: "24px",
+    color: "green",
+  },
+  menu: {
+    display: "flex",
+    justifyContent: "center",
+    listStyle: "none",
+    padding: "0",
+    margin: "10px 0",
+  },
+  menuItem: {
+    padding: "10px",
+  },
+  "@media (max-width: 600px)": {
+    heading: {
+      fontSize: "24px",
+    },
+    paragraph: {
+      fontSize: "20px",
+    },
+  },
+};
+
+const Home = () => (
+  <div style={styles.container}>
+    <h1 style={styles.heading}>Home</h1>
+    <p style={styles.paragraph}>Welcome to the home page.</p>
+  </div>
+);
+
+const About = () => (
+  <div style={styles.container}>
+    <h1 style={styles.heading}>About</h1>
+    <p style={styles.paragraph}>
+      Hello, thanks for visiting hairgrowthcare! Right now the site is under
+      construction! Please check back soon!
+    </p>
+  </div>
+);
+
+const Contact = () => (
+  <div style={styles.container}>
+    <h1 style={styles.heading}>Contact</h1>
+    <p style={styles.paragraph}>You can contact us at contact@example.com.</p>
+  </div>
+);
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">this is a test22</header>
+    <div>
+      <Router>
+        <div>
+          <ul style={styles.menu}>
+            <li style={styles.menuItem}>
+              <Link to="/">Home</Link>
+            </li>
+            <li style={styles.menuItem}>
+              <Link to="/about">About</Link>
+            </li>
+            <li style={styles.menuItem}>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+
+          <Routes>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+          </Routes>
+        </div>
+      </Router>
+      <About></About>
     </div>
   );
-}
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 export default App;
