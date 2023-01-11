bf1b5fe4e3b0d1fd92b1023bc23790bf17dffb32
umbur <work1415@gmail.com>
2023-01-10 19:07:22 -0700

    test2

--- hairgrowthcare/src/App.js
+++ hairgrowthcare/src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">this is a test</header>
+      <header className="App-header">this is a test22</header>
     </div>
   );
 }
