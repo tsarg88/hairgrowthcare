e5fca101bfead5b99c1c641886befd3335ba4e28
umbur <work1415@gmail.com>
2023-01-22 22:51:35 -0700

    added greeting and mobile support to contact page

--- hairgrowthcare/src/Contact/Contact.js
+++ hairgrowthcare/src/Contact/Contact.js
@@ -6,36 +6,50 @@ const Contact = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
-
+  const [error, setError] = useState({}); // added
   const [loader, setLoader] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setLoader(true);
+    // added form validation
+    if (!name) {
+      setError({ name: "Name is required" });
+    } else if (!email) {
+      setError({ email: "Email is required" });
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
+      setError({ email: "Invalid email format" });
+    } else if (!message) {
+      setError({ message: "Message is required" });
+    } else {
+      setLoader(true);
+      setError({}); // added
 
-    db.collection("contacts")
-      .add({
-        name: name,
-        email: email,
-        message: message,
-      })
-      .then(() => {
-        setLoader(false);
-        alert("Your message has been submitted👍");
-      })
-      .catch((error) => {
-        alert(error.message);
-        setLoader(false);
-      });
+      db.collection("contacts")
+        .add({
+          name: name,
+          email: email,
+          message: message,
+        })
+        .then(() => {
+          setLoader(false);
+          alert("Thanks! Your message has been received!");
+        })
+        .catch((error) => {
+          alert(error.message);
+          setLoader(false);
+        });
 
-    setName("");
-    setEmail("");
-    setMessage("");
+      setName("");
+      setEmail("");
+      setMessage("");
+    }
   };
-
   return (
     <form className="form" onSubmit={handleSubmit}>
-      <h1>Hello, thanks again for visiting hairgrowthcare.com!</h1>
+      <h1>
+        Hello, thanks again for visiting{" "}
+        <span id="siteName">hairgrowthcare.com</span>!
+      </h1>
       <h2>Please fill the below form and we will get back to you asap!</h2>
       <label>Name</label>
       <input
@@ -43,13 +57,14 @@ const Contact = () => {
         value={name}
         onChange={(e) => setName(e.target.value)}
       />
-
+      {error.name && <p style={{ color: "red" }}>{error.name}</p>}
       <label>Email</label>
       <input
         placeholder="Email"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
       />
+      {error.email && <p style={{ color: "red" }}>{error.email}</p>}
 
       <label>Message</label>
       <textarea
@@ -57,6 +72,7 @@ const Contact = () => {
         value={message}
         onChange={(e) => setMessage(e.target.value)}
       ></textarea>
+      {error.message && <p style={{ color: "red" }}>{error.message}</p>}
 
       <button
         type="submit"
