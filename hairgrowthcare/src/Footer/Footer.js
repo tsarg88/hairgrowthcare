0f92841cffeacab01c968142778c9942ba5d11e6
umbur <work1415@gmail.com>
2023-01-23 00:20:58 -0700

    contact page with nav

--- hairgrowthcare/src/Footer/Footer.js
+++ hairgrowthcare/src/Footer/Footer.js
@@ -1,10 +1,18 @@
 import React from "react";
 import "./Footer.css";
+import { Route, Routes, Link } from "react-router-dom";
 
 export const Footer = () => (
   <footer>
     <p>As an Amazon Associate I may earn from qualifying purchases</p>
     <p>Copyright &copy; {new Date().getFullYear()} hairgrwothcare.com</p>
+    <p id="contactMe">
+      Please feel free to{" "}
+      <Link to="/contact" target="_blank">
+        <a> Contact</a>
+      </Link>{" "}
+      me if you have any questions/concerns!
+    </p>
   </footer>
 );
 
