c6c0e45c2a3bbb2473635d51b63019b2762f4b3c
umbur <work1415@gmail.com>
2023-02-17 21:37:17 -0700

    testing style

--- hairgrowthcare/src/pages/Solutions.js
+++ hairgrowthcare/src/pages/Solutions.js
@@ -213,7 +213,7 @@ export const Solutions = () => {
               frameborder="0"
               src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ac&ref=qf_sp_asin_til&ad_type=product_link&tracking_id=hatro-20&marketplace=amazon&region=US&placement=B0994YMK1Z&asins=B0994YMK1Z&linkId=2747db84071ed549b5d81ff7076af287&show_border=true&link_opens_in_new_window=true&price_color=333333&title_color=0066c0&bg_color=ffffff"
             ></iframe>
-            {/*  test */}
+            {/*  tes */}
           </div>
         </body>
       </div>
