cac99ae909617535e42d1c0b8ceb4536c5fcbb09
umbur <work1415@gmail.com>
2023-06-01 16:31:43 -0600

    fixing ad bug

--- hairgrowthcare/src/pages/Solutions.js
+++ hairgrowthcare/src/pages/Solutions.js
@@ -93,9 +93,9 @@ export const Solutions = () => {
               <iframe
                 sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin"
                 style={{ width: "120px", height: "240px" }}
-                marginwidth="0"
-                marginheight="0"
-                scrolling="no"
+                // marginwidth="0"
+                // marginheight="0"
+                // scrolling="no"
                 frameborder="0"
                 src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ss&ref=as_ss_li_til&ad_type=product_link&tracking_id=hgc03-20&language=en_US&marketplace=amazon&region=US&placement=B078GYC6QN&asins=B078GYC6QN&linkId=73c4b54ed85527572c9dcd652a914379&show_border=true&link_opens_in_new_window=true"
               ></iframe>
